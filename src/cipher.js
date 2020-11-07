7d4c1febcfc7ddb8843dd2dab9e5a99854f8e344
Priscila Silveira <priscilassilveira97@gmail.com>
2020-11-06 22:38:00 -0300

    separação de modulos

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,55 @@
-const cipher = {
-  // ...
-};
+//recebe o botao e adiciona um evento que recebe a function
 
-export default cipher;
+
+function encode(offset, frase) {
+
+    let criptografar
+    let textoFinal = ""
+
+    if (frase == null || offset == null || frase == "" || offset == "") {
+        throw new TypeError("ops... digite apenas letras")
+
+    }
+    else {
+        for (let index = 0; index < frase.length; index++) {
+
+            const pegaNum = (frase.charCodeAt(index) - 65 + offset) % 26 + 65;
+
+            criptografar = String.fromCharCode(pegaNum)
+            textoFinal += criptografar
+
+        }
+    }
+
+
+
+    return textoFinal
+    //document.write(type(criptografar))
+}
+
+
+
+function decode(offset, frase) {
+
+    let descriptografar
+    let textoFinal = ""
+    if (frase == null || offset === null || frase == "" || offset === "") {
+        throw new TypeError("ops... digite apenas letras")
+    }
+    else {
+
+        for (let index = 0; index < frase.length; index++) {
+
+            const pegaNum = (frase.charCodeAt(index) + 65 - offset) % 26 + 65;
+
+            descriptografar = String.fromCharCode(pegaNum)
+            textoFinal += descriptografar
+        }
+
+    }
+    return textoFinal
+
+}
+export default {
+    encode, decode
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import cipher from './cipher.js';
+function clickEncode(event) {
+    
+    event.preventDefault()
+    let frase = document.getElementById("texto").value
+    frase = frase.toUpperCase()
+    let offset = parseInt(document.getElementById("deslocamento").value)
+    let retorna = cipher.encode(frase,offset);
+    
+    document.getElementById("texto").value = retorna;
+}
 
-console.log(cipher);
+document.getElementById("myBtn").addEventListener("click", clickEncode)
+
+function clickDecode(event) { 
+    event.preventDefault()
+    let frase = document.getElementById("texto").value
+    frase = frase.toUpperCase()
+    let offset = parseInt(document.getElementById("deslocamento").value)
+    let retornaD= cipher.decode(frase,offset);
+
+    document.getElementById("texto").value = retornaD;
+}
+
+document.getElementById("dBtn").addEventListener("click", clickDecode)
+//console.log(cipher);
+
+
+
+
